54633e0be8b5f1caf587f5d88304563a6c979a27
Joseph Kohlmann <kohlmannj@gmail.com>
2017-05-05 16:17:24 -0400

    Remove semicolons from source file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
  * Imports
  */
 
-import { create } from 'jss';
-import preset from 'jss-preset-default';
+import { create } from 'jss'
+import preset from 'jss-preset-default'
 
 /**
  * Constants
  */
 
-let jss = create(preset());
-let sheets = [];
-let map = {};
+let jss = create(preset())
+let sheets = []
+let map = {}
 
 /**
  * JSS Simple
@@ -19,65 +19,71 @@ let map = {};
 
 const defaultOpts = {
   meta: 'jss-simple'
-};
+}
 
 function css(style, opts, key) {
-  let localKey = key;
-  let localOpts;
+  let localKey = key
+  let localOpts
 
   if (typeof opts === 'object' && opts !== null) {
     // Merge with defaultOpts
     localOpts = {
       ...defaultOpts,
       ...opts
-    };
+    }
   }
 
   else if (typeof opts === 'string') {
-    localKey = opts;
-    localOpts = defaultOpts;
+    localKey = opts
+    localOpts = defaultOpts
   }
 
-  const sheet = jss.createStyleSheet(style, localOpts);
+  const sheet = jss.createStyleSheet(style, localOpts)
 
   if (localKey !== undefined) {
     if (map[localKey] !== undefined) {
-      sheets[map[localKey]] = sheet;
-      return sheet.classes;
+      sheets[map[localKey]] = sheet
+      return sheet.classes
     }
 
-    map[localKey] = sheets.length;
+    map[localKey] = sheets.length
   }
 
-  sheets.push(sheet);
-  return sheet.classes;
+  sheets.push(sheet)
+  return sheet.classes
 }
 
 function attach() {
-  return sheets.forEach(sheet => sheet.attach());
+  return sheets.forEach(sheet => sheet.attach())
 }
 
 function detach() {
-  return sheets.forEach(sheet => sheet.detach());
+  return sheets.forEach(sheet => sheet.detach())
 }
 
 function toString() {
-  return sheets.map(sheet => sheet.toString()).join('\n');
+  return sheets.map(sheet => sheet.toString()).join('\n')
 }
 
 function use(plugin) {
-  jss.use(plugin);
-  return { use, toString, attach };
+  jss.use(plugin)
+  return { use, toString, attach }
 }
 
 function clear() {
-  sheets = [];
-  map = {};
+  sheets = []
+  map = {}
 }
 
 /**
  * Exports
  */
 
-export default css;
-export { use, toString, attach, detach, clear };
+export default css
+export {
+  use,
+  toString,
+  attach,
+  detach,
+  clear
+}
